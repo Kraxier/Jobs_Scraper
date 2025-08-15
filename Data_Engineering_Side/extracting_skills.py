adda34e1ab54aa964cab3ca5e2a5bae529ecbd5d
Kyla Antonette Binas <binaskristian@gmail.com>
2025-08-16 04:04:35 +0800

    I need NLP and also to read the CSV files it require learning some pandas but focusing on the project i can focus on the necessary one Anchor Learning

--- Data_Engineering_Side/extracting_skills.py
+++ Data_Engineering_Side/extracting_skills.py
@@ -21,19 +21,96 @@ python -m spacy download en_core_web_md   # medium
 python -m spacy download en_core_web_lg   # large
 '''
 
-import spacy
+# # Importing spaCy
+# import spacy
 
-# Load the English model
+# # Imports spaCy’s PhraseMatcher, a fast rule-based matcher for finding exact phrases (token sequences) in a Doc.
+# from spacy.matcher import PhraseMatcher
+
+# Importing Pandas in my Script 
+import pandas as pd
+
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+
+# 1. Load your CSV file
+df = pd.read_csv("mechatronics_jobs_2025-08-13.csv")
+# print(df) Printing out the csv file thing 
+
+# 2. Load spaCy model
 nlp = spacy.load("en_core_web_sm")
 
-# Text to analyze
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion.")
+# 3. Define the keywords you want to find
+terms = [
+    "abb robotics",
+    "arduino",
+    "autocad electrical",
+    "control valves",
+    "cnc programming",
+    "daq",
+    "data acquisition systems",
+    "dcs",
+    "distributed control system",
+    "eplan electric p8",
+    "embedded systems",
+    "ethernet/ip",
+    "fanuc robotics",
+    "fieldbus protocols",
+    "flowmeters",
+    "foundation fieldbus",
+    "hart protocol",
+    "hmi",
+    "human-machine interface",
+    "hydraulics systems",
+    "ignition scada",
+    "industrial networking",
+    "instrument calibration",
+    "i/o module configuration",
+    "kepware opc server",
+    "kuka robotics",
+    "ladder logic",
+    "loop checking & commissioning",
+    "machine vision systems",
+    "matlab",
+    "simulink",
+    "mechatronics design",
+    "modbus",
+    "motion control systems",
+    "opc da",
+    "opc ua",
+    "pid control",
+    "plc",
+    "programmable logic controller",
+    "pneumatics systems design",
+    "process instrumentation",
+    "process safety systems",
+    "sis",
+    "profibus",
+    "proximity sensors",
+    "rslogix",
+    "studio 5000",
+    "rtd sensors",
+    "raspberry pi",
+    "scada",
+    "supervisory control and data acquisition",
+    "sensor integration",
+    "servo motor control",
+    "signal conditioning",
+    "siemens tia portal",
+    "solidworks",
+    "stm32 microcontroller",
+    "thermocouples",
+    "vfd programming",
+    "wonderware intouch"
+]
 
-# Print named entities
-for ent in doc.ents:
-    print(ent.text, ent.label_)
 
 
+# 4. Create the PhraseMatcher
+# matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
+# patterns = [nlp.make_doc(t) for t in terms]
+# matcher.add("TECH_TERMS", patterns)
 
 
 
@@ -81,3 +158,48 @@ Some common tasks in NLP include:
     Chatbots & virtual assistants – understanding your questions and responding.
     Summarization – condensing large documents into shorter summaries
 '''
+
+# What is Pandas in python?
+r'''
+is a powerful open-source data analysis and manipulation library.
+It’s built on top of NumPy and is designed to make working with structured data (like tables, spreadsheets, or databases) easier.
+'''
+# What are Key Things about Pandas?
+r'''
+Name origin: It comes from “Panel Data,” a term for multidimensional structured datasets in statistics.
+
+Core objects:
+    Series → A one-dimensional labeled array (like a column in Excel).
+    DataFrame → A two-dimensional labeled data structure (like a spreadsheet or SQL table).
+'''
+
+# What are Feature of Pandas?
+r'''
+Features:
+    * Reading/writing data from CSV, Excel, SQL, JSON, etc.
+    * Data cleaning (handling missing values, renaming columns, dropping duplicates).
+    * Powerful data selection and filtering (loc, iloc).
+    * Grouping, aggregating, and summarizing data.
+    * Time series handling.
+'''
+
+# How to Install Pandas?
+r'''
+pip install pandas
+'''
+
+
+
+# Simple Program:
+# Importing spaCy
+# import spacy
+
+# Load the English model
+# nlp = spacy.load("en_core_web_sm")
+
+# # Text to analyze
+# doc = nlp("Apple is looking at buying U.K. startup for $1 billion.")
+
+# # Print named entities
+# for ent in doc.ents:
+#     print(ent.text, ent.label_)
