b85e57a729fbf872383933cbeb2ce5c9b9d6d93f
Kyla Antonette Binas <binaskristian@gmail.com>
2025-08-21 17:46:14 +0800

    Learning the concept of README not realy learning but gathering the information to read about how to write the readme and maybe the workflow of it adding it in the system of workflow

--- Proper_Project_Structure/concept_project_sturcture.py
+++ Proper_Project_Structure/concept_project_sturcture.py
@@ -52,3 +52,93 @@ data/raw/ vs data/processed/ → Separates raw HTML from cleaned datasets (impor
 proxy_manager.py → Real-world scraping often needs rotating proxies & user-agent pools.
 docs/ → Good if working with clients/teams.
 '''
+
+
+
+# What to Write in README.md 
+r'''
+What is README? 
+    -  README is a text file that introduces and explains a project. It contains information that is commonly required to understand what the project is about.
+    In simple words, we can describe a README file as a guide that gives users a detailed description of a project you have worked on.
+    It can also be described as documentation with guidelines on how to use a project. Usually it will have instructions on how to install and run the project.
+
+Why should I make it?
+    It's an easy way to answer questions that your audience will likely have regarding how to install and use your project and also how to collaborate with you.
+    It is the first file a person will see when they encounter your project, so it should be fairly brief but detailed.
+
+Who should make it?
+    Anyone who is working on a programming project, especially if you want others to use it or contribute.
+
+When should I make it?
+    Definitely before you show a project to other people or make it public. You might want to get into the habit of making it the first file you create in a new project.
+
+Where should I put it?
+    In the top level directory of the project. This is where someone who is new to your project will start out. Code hosting services such as GitHub, Bitbucket, and GitLab will also look for your README and display it along with the list of files and directories in your project.
+
+What i needed to think about when Writing a README? 
+Before we get started, it is also important to note that when you're writing your project's README, it should be able to answer the what, why, and the how of the project.
+
+Here are some guide questions that will help you out:
+    What was your motivation?
+    Why did you build this project?
+    What problem does it solve?
+    What did you learn?
+    What makes your project stand out? If your project has a lot of features, consider adding a "Features" section and listing them here.
+
+'''
+
+##############################
+r'''
+What to Include in your README
+1. Project's Title
+This is the name of the project. It describes the whole project in one sentence, and helps people understand what the main goal and aim of the project is.
+
+2. Project Description
+This is an important component of your project that many new developers often overlook.
+Your description is an extremely important aspect of your project. A well-crafted description allows you to show off your work to other developers as well as potential employers.
+The quality of a README description often differentiates a good project from a bad project. A good one takes advantage of the opportunity to explain and showcase:
+What your application does,
+Why you used the technologies you used,
+Some of the challenges you faced and features you hope to implement in the future.
+
+3. Table of Contents (Optional)
+If your README is very long, you might want to add a table of contents to make it easy for users to navigate to different sections easily. It will make it easier for readers to move around the project with ease.
+
+4. How to Install and Run the Project
+If you are working on a project that a user needs to install or run locally in a machine like a "POS", you should include the steps required to install your project and also the required dependencies if any.
+Provide a step-by-step description of how to get the development environment set and running.
+
+5. How to Use the Project
+Provide instructions and examples so users/contributors can use the project. This will make it easy for them in case they encounter a problem – they will always have a place to reference what is expected.
+You can also make use of visual aids by including materials like screenshots to show examples of the running project and also the structure and design principles used in your project.
+Also if your project will require authentication like passwords or usernames, this is a good section to include the credentials.
+
+6. Include Credits
+If you worked on the project as a team or an organization, list your collaborators/team members. You should also include links to their GitHub profiles and social media too.
+Also, if you followed tutorials or referenced a certain material that might help the user to build that particular project, include links to those here as well.
+This is just a way to show your appreciation and also to help others get a first hand copy of the project.
+
+7. Add a License
+For most README files, this is usually considered the last part. It lets other developers know what they can and cannot do with your project.
+We have different types of licenses depending on the kind of project you are working on. Depending on the one you will choose it will determine the contributions your project gets.
+The most common one is the GPL License which allows other to make modification to your code and use it for commercial purposes. If you need help choosing a license, use check out this link: https://choosealicense.com/
+Up to this point what we have covered are the minimum requirements for a good README. But you might also want to consider adding the following sections to make it more eye catching and interactive.
+
+Additional README Sections
+8. Badges
+Badges aren't necessary, but using them is a simple way of letting other developers know that you know what you're doing.
+Having this section can also be helpful to help link to important tools and also show some simple stats about your project like the number of forks, contributors, open issues etc...
+Below is a screenshot from one of my projects that shows how you can make use of badges:
+badges
+The good thing about this section is that it automatically updates it self.
+Don't know where to get them from? Check out the badges hosted by shields.io. They have a ton of badges to help you get started. You may not understand what they all represent now, but you will in time.
+
+9. How to Contribute to the Project
+This mostly will be useful if you are developing an open-source project that you will need other developers to contribute to. You will want to add guidelines to let them know how they can contribute to your project.
+Also it is important to make sure that the licence you choose for an open-source projects is correct to avoid future conflicts. And adding contribution guidelines will play a big role.
+Some of the most common guidelines include the Contributor Covenant and the Contributing guide. Thes docs will give you the help you need when setting rules for your project.
+
+10. Include Tests
+Go the extra mile and write tests for your application. Then provide code examples and how to run them.
+This will help show that you are certain and confident that your project will work without any challenges, which will give other people confidence in it, too
+'''
